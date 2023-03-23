b54791118d023d7e760fce70a9bbf441dad8994f
KaseRadtke <KaseRadtke@comcast.net>
2023-03-22 20:41:51 -0500

    Firebase init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { AngularFireMessaging } from '@angular/fire/compat/messaging';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'DispatchOscar';
+  constructor(private messaging: AngularFireMessaging) {
+    this.requestPermission();
+  }
+
+  requestPermission() {
+    this.messaging.requestToken.subscribe(
+      (token) => {
+        console.log('Permission granted! Save to the server!', token);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { AngularFireModule} from '@angular/fire/compat'
+import { AngularFireMessagingModule } from '@angular/fire/compat/messaging';
 import { AppComponent } from './app.component';
+import {environment} from "../environments/environments";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment),
+    AngularFireMessagingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
